b07ebfd58a323ccf8d96b1e331de89c6a9dc6bb8
Hemang P <60398246+Hemang0710@users.noreply.github.com>
2025-06-01 11:35:34 -0400

    Lab_4

--- nextjs-dashboard/app/dashboard/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-        return <p>Dashboard Page Of Hemang Patel Application</p>;
-}
--- nextjs-dashboard/next.config.ts
+++ nextjs-dashboard/next.config.ts
@@ -1,18 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has type errors.
-    ignoreBuildErrors: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-  },
+import type { NextConfig } from 'next';
+ 
+const nextConfig: NextConfig = {
+  experimental: {
+    ppr: 'incremental'
+  }
 };
-
-module.exports = nextConfig;
+ 
+export default nextConfig;
